162e4b67e87d22b766687d1c7ac1fbd622c17fa6
arathipremkumar <57580997+arathipremkumar@users.noreply.github.com>
2023-12-11 18:42:27 +0530

    Update integrated_functions.py

--- integrated_functions.py
+++ integrated_functions.py
@@ -103,7 +103,7 @@ def summ(prompt,outputofaisearch):
   completion = openai.ChatCompletion.create(
     engine="Summarizer2",
     messages = message_text,
-    temperature=0.4,
+    temperature=0.3,
     max_tokens=800,
     top_p=0.95,
     frequency_penalty=0,
